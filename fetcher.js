23f605f08bde1745b70b7b7bb48a539513d911ac
FuzzyKittyz <topher.a@hotmail.com>
2022-09-07 13:11:29 -0600

    takes a url and downloads into a file all using the command line

---
+++ fetcher.js
@@ -0,0 +1,16 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, (error, response, body) => {
+  console.log('error:', error);
+  fs.writeFile(`${path}`, body, function(error) {
+    if (error) {
+      console.log(error)
+    } else {
+        console.log(`Downloaded and saved ${response.headers["content-length"]} to ${path}`)
+    }
+  });
+});
